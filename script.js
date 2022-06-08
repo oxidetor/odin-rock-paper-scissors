a376971e5bb25da669c2d6c097e0c8f8306aa42e
oxidetor <prateek.girdhar@outlook.com>
2022-06-08 13:54:18 -0700

    Add starting pseudocode to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// declare function computerPlay that takes no inputs
+// inside this function initialize an integer variable randNumber
+// generate a random number (either 0, 1, or 2) and store it in randNumber
+// return "Rock" if randNumber is 0, "Paper" if randNumber is 1, "Scissors" if randNumber is 2
+
+// declare function playRound that takes two parameters - playerSelection, computerSelection
+// modify the playerSelection string to use Sentence case capitalization
+// calculate whose selection "wins"
+// return a string that declares the winner of the round
