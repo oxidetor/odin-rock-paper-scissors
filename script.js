8fe45925b10e94b61f91a986ad227dd5acf88a28
oxidetor <prateek.girdhar@outlook.com>
2022-06-08 14:22:59 -0700

    Create computerPlay and playRound functions

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 // declare function computerPlay that takes no inputs
-// inside this function initialize an integer variable randNumber
-// generate a random number (either 0, 1, or 2) and store it in randNumber
-// return "Rock" if randNumber is 0, "Paper" if randNumber is 1, "Scissors" if randNumber is 2
+function computerPlay() {
+  // inside this function initialize an integer variable randNumber
+  // generate a random number (either 0, 1, or 2) and store it in randNumber
+  let randNumber = Math.floor(Math.random() * 3);
+
+  // return "Rock" if randNumber is 0, "Paper" if randNumber is 1, "Scissors" if randNumber is 2
+  switch (randNumber) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+    default:
+      break;
+  }
+}
 
 // declare function playRound that takes two parameters - playerSelection, computerSelection
-// modify the playerSelection string to use Sentence case capitalization
-// calculate whose selection "wins"
-// return a string that declares the winner of the round
+function playRound(playerSelection, computerSelection) {
+  // modify the playerSelection string to use Sentence case capitalization
+  playerSelection = `${playerSelection
+    .slice(0, 1)
+    .toUpperCase()}${playerSelection.slice(1).toLowerCase()}`;
+
+  // calculate whose selection "wins" and return a string that declares the winner
+  if (playerSelection == computerSelection) return "Tie";
+  else if (playerSelection == "Rock" && computerSelection == "Paper")
+    return "You Lose! Paper beats Rock";
+  else if (playerSelection == "Paper" && computerSelection == "Rock")
+    return "You Win! Paper beats Rock";
+  else if (playerSelection == "Scissors" && computerSelection == "Rock")
+    return "You Lose! Rock beats Scissors";
+  else if (playerSelection == "Rock" && computerSelection == "Scissors")
+    return "You Win! Rock beats Scissors";
+  else if (playerSelection == "Paper" && computerSelection == "Scissors")
+    return "You Lose! Scissors beats Paper";
+  else if (playerSelection == "Scissors" && computerSelection == "Paper")
+    return "You Win! Scissors beats Paper";
+  else return "Something went wrong";
+}
